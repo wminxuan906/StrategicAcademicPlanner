fef902faf0a49545eab225fe350f25573ff1ff37
Minxuan Wang <3152167W@student.gla.ac.uk>
2026-08-15 01:05:45 +0800

    Clean up JavaScript and analytics rendering

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,23 @@
 console.log("Strategic Academic Planner loaded.");
+
+document.querySelectorAll(".js-height-bar").forEach(function (bar) {
+    bar.style.height = bar.dataset.height + "%";
+});
+
+document.querySelectorAll(".js-width-bar").forEach(function (bar) {
+    bar.style.width = bar.dataset.width + "%";
+});
+
+// Reset expected marks in the What-if Calculator
+const resetButton = document.getElementById("resetScenarioInputs");
+
+if (resetButton) {
+    resetButton.addEventListener("click", function () {
+        const expectedMarkInputs =
+            document.querySelectorAll('input[name^="expected_"]');
+
+        expectedMarkInputs.forEach(function (input) {
+            input.value = "";
+        });
+    });
+}
